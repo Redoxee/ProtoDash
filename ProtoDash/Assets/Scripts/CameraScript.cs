0212a5f4212c92a3fa15f253e35e29636969f146
Anton Roy <anton.francis.roy@gmail.com>
2016-06-19 02:17:23 +0200

    improving the camera on x axis

--- ProtoDash/Assets/Scripts/CameraScript.cs
+++ ProtoDash/Assets/Scripts/CameraScript.cs
@@ -4,6 +4,10 @@ using Assets;
 public class CameraScript : MonoBehaviour {
 	[SerializeField]
 	private GameObject target;
+
+    [SerializeField]
+    MainScript mainScriptRef;
+
 	[SerializeField]
 	private float xDampingFactor = 0.5f;
 	[SerializeField]
@@ -12,23 +16,67 @@ public class CameraScript : MonoBehaviour {
 	[SerializeField]
 	private float cameraYOffset = 6;
 	[SerializeField]
-	private float wantedXOffset = 3;
+	private float wantedXOffset = 6;
+
+
+    [SerializeField]
+    private float timeToPosX = 1.5f;
+    [SerializeField]
+    private AnimationCurve repositionXCurve;
 
 	private float originalZoom;
+    private float lastOffsetX = 0.0f;
+    private float lastOrientation = 0.0f;
+    private float timerPosX = 0.0f;
+
 
 	void Start () {
 		originalZoom = transform.position.z;
 	}
 
-	private Vector3 _Damping(Vector3 p, Vector3 t, float d, float dt)
-	{
-		return p + (t - p) / Mathf.Max(d,float.Epsilon) * dt;
-	}
+    void Update()
+    {
+        if (lastOrientation != mainScriptRef.getFacingSign())
+        {
+            lastOrientation = mainScriptRef.getFacingSign();
+            timerPosX = timeToPosX;
+            lastOffsetX = transform.position.x - target.transform.position.x;
+            Debug.Log("cpx = " + transform.position.x + "| tpx = " + target.transform.position.x + " | cox  = " + lastOffsetX + "| facing = " + lastOrientation);
+        }
+        if (timerPosX > 0)
+        {
+            timerPosX = Mathf.Max(0, timerPosX - Time.deltaTime);
+        }
+    }
+
+    private void _DrawCross(float x, float y, Color col)
+    {
+        Debug.DrawRay(new Vector3(x - .5f, y, 0.0f), new Vector3(1f, 0.0f, 0.0f), col);
+        Debug.DrawRay(new Vector3(x, y - .5f, 0.0f), new Vector3(0f, 1f, 0.0f), col);
+    }
 
 	void LateUpdate()
 	{
-		Vector3 tp = target.transform.position + new Vector3(wantedXOffset,cameraYOffset,originalZoom );
-		tp.z = originalZoom;
-		transform.position = new Vector3(FuctionUtils.damping(xDampingFactor ,transform.position.x,tp.x,Time.deltaTime),FuctionUtils.damping(yDampingFactor, transform.position.y, tp.y, Time.deltaTime),transform.position.z);
+        float orientation =  mainScriptRef.getFacingSign();
+        float currentOffsetX = wantedXOffset * orientation;
+        if (timerPosX > 0)
+        {
+            float progression = 1.0f - repositionXCurve.Evaluate(1.0f - timerPosX / timeToPosX);
+
+            currentOffsetX = currentOffsetX + (lastOffsetX - currentOffsetX) * progression;
+            orientation = lastOrientation;
+
+        }
+
+        Vector3 p = target.transform.position;
+        float tpx = p.x + currentOffsetX;
+        float tpy = p.y + cameraYOffset;
+        _DrawCross(tpx, tpy, Color.yellow);
+        tpx = FuctionUtils.damping(xDampingFactor, transform.position.x, tpx, Time.deltaTime);
+        tpy = FuctionUtils.damping(yDampingFactor, transform.position.y, tpy, Time.deltaTime);
+        _DrawCross(p.x + wantedXOffset * orientation, p.y + cameraYOffset, Color.gray);
+        _DrawCross(tpx, tpy, Color.cyan);
+
+        transform.position = new Vector3(tpx, tpy, transform.position.z);
 	}
 }
