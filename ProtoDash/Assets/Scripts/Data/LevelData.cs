8c4926dd443c55ed0d4b20846cfb2cebaff7c0e0
Redoxee <anton.francis.roy@gmail.com>
2016-08-20 13:58:39 +0200

    Starting a tool chain to make level creation easier

---
+++ ProtoDash/Assets/Scripts/Data/LevelData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu()]
+public class LevelData : ScriptableObject {
+
+	public Object sceneObject;
+	public string worldLabel;
+	public string levelLabel;
+	public float parTime;
+}
---
+++ ProtoDash/Assets/Scripts/Editor/BuildTools.cs
@@ -0,0 +1,44 @@
+﻿//#define CREATE_FLOW
+
+using UnityEngine;
+using UnityEditor;
+
+public class BuildTools : EditorWindow
+{
+	private const string LEVEL_FLOW_PATH = "Assets/Data/";
+	private const string DEFAULT_FLOW_NAME = "LevelFlow.Asset";
+	private const string MAIN_FLOW_NAME = "MainLevelFlow.Asset";
+	private LevelFlow mainLevelFlow;
+
+	#region Create flow
+#if CREATE_FLOW
+	[MenuItem("Dasher/Create level flow")]
+	static void DoSomething()
+	{
+		Debug.Log("Creating a new level flow");
+		LevelFlow lf = new LevelFlow();
+		AssetDatabase.CreateAsset(lf, LEVEL_FLOW_PATH + DEFAULT_FLOW_NAME);
+		Debug.Log("Level flow created");
+	}
+#endif
+	#endregion
+
+	[MenuItem("Dasher/BuildTool")]
+	public static void ShowWindow()
+	{
+		EditorWindow.GetWindow(typeof(BuildTools));
+	}
+
+	void OnGUI()
+	{
+		if (GUILayout.Button("Set Scenes"))
+		{
+			SetSceneInProjects();
+		} 
+	}
+
+	void SetSceneInProjects()
+	{
+
+	}
+}
