fb9cc0a1d407af77906dfb0f411eef974cb50d4e
Redoxee <anton.francis.roy@gmail.com>
2016-11-15 02:25:50 +0100

    several improvement, starting UI Refont

---
+++ ProtoDash/Assets/Scripts/GUI/TimeDisplayCapsule.cs
@@ -0,0 +1,228 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+
+namespace Dasher
+{
+	public class TimeDisplayCapsule : MonoBehaviour
+	{
+		[Header("Components")]
+		[SerializeField]
+		GameObject m_mainText = null;
+		Text m_mainTextText;
+
+		[SerializeField]
+		GameObject m_additionalText = null;
+		Text m_additionalTextText;
+
+		[SerializeField]
+		GameObject m_frontPanel = null;
+
+		[SerializeField]
+		GameObject m_backPanel = null;
+
+		[SerializeField]
+		GameObject m_flashPanel = null;
+		Image m_flashImage = null;
+		Color m_flashImageColor = Color.white;
+
+		[Header("Flash")]
+		[SerializeField]
+		float m_flashDuration = 1f;
+		float m_flashTimer = 0f;
+
+		[SerializeField]
+		[Range(0,1)]
+		float m_hitTime = .5f;
+
+		[SerializeField]
+		AnimationCurve m_flashCurve = null;
+
+		Action m_impactAction = null;
+
+		[Header("Slide")]
+		[SerializeField]
+		float m_slideAnimationDuration = 1f;
+		float m_slideAnimationTimer = 0f;
+
+		[SerializeField]
+		AnimationCurve m_slideAnimationCurve = null;
+
+		[Header("Colors")]
+		[SerializeField]
+		Material m_badMaterial = null;
+
+		[SerializeField]
+		Material m_neutralMaterial = null;
+
+		[SerializeField]
+		Material m_goodMaterial = null;
+
+
+		Image m_frontBorderImage = null;
+		Image m_backBorderImage = null;
+
+		float m_slideEndOffset = 0f;
+
+		public void SetMainText(string message)
+		{
+			m_mainTextText.text = message;
+		}
+
+		public void SetAdditionalText(string message)
+		{
+			m_additionalTextText.text = message;
+		}
+
+		public void SetTexts(string messageMain,string messageAdditional)
+		{
+			SetMainText(messageMain);
+			SetAdditionalText(messageAdditional);
+		}
+
+		#region Flash
+
+		public void StartFlash(Action impactAction = null)
+		{
+			m_flashTimer = 0f;
+			m_flashImage.enabled = true;
+			m_flashImageColor.a = 0f;
+			m_flashImage.color = m_flashImageColor;
+
+			m_impactAction = impactAction;
+		}
+
+		public void FlashInText(string message)
+		{
+			StartFlash(() =>
+			{
+				SetMainText(message);
+			});
+		}
+
+		public void EndFlash()
+		{
+			m_flashImage.enabled = false;
+			m_flashTimer = m_flashDuration + 1f;
+		}
+		#endregion
+
+		#region Slide
+
+		public void HideBackPanel()
+		{
+			EndSlideAnimation();
+			m_backPanel.gameObject.SetActive(false);
+		}
+
+		public void StartSlide()
+		{
+			m_slideAnimationTimer = 0f;
+			m_backPanel.transform.localPosition = new Vector3(0, 0, 0);
+			m_backPanel.gameObject.SetActive(true);
+		}
+
+		public void EndSlideAnimation()
+		{
+			m_slideAnimationTimer = m_slideAnimationTimer + 1f;
+		}
+
+		#endregion
+
+
+		public void Initialize()
+		{
+			m_mainTextText = m_mainText.GetComponent<Text>();
+			m_additionalTextText = m_additionalText.GetComponent<Text>();
+			m_flashImage = m_flashPanel.GetComponent<Image>();
+			m_flashImageColor = m_flashImage.color;
+
+			m_slideEndOffset = m_backPanel.transform.localPosition.y;
+
+			EndFlash();
+
+			HideBackPanel();
+
+			m_frontBorderImage = m_frontPanel.GetComponent<Image>();
+			m_backBorderImage = m_backPanel.GetComponent<Image>();
+		}
+
+		public void ManualUpdate()
+		{
+			float dt = Time.deltaTime;
+			if (m_flashTimer < m_flashDuration)
+			{
+				var wasUnder = (m_flashTimer / m_flashDuration < m_hitTime);
+				m_flashTimer += dt;
+				var isUpper = (m_flashTimer / m_flashDuration >= m_hitTime);
+				m_flashImageColor.a = m_flashCurve.Evaluate(m_flashTimer / m_flashDuration);
+				m_flashImage.color = m_flashImageColor;
+
+				if (wasUnder && isUpper && m_impactAction != null)
+				{
+					m_impactAction();
+				}
+
+				if (m_flashTimer >= m_flashDuration)
+				{
+					EndFlash();
+				}
+			}
+
+			if (m_slideAnimationTimer < m_slideAnimationDuration)
+			{
+				m_slideAnimationTimer += dt;
+
+				float p = Mathf.Clamp01(m_slideAnimationTimer / m_slideAnimationDuration);
+				p = m_slideAnimationCurve.Evaluate(p);
+				var pos = m_backPanel.transform.localPosition;
+				pos.y = m_slideEndOffset * p;
+				m_backPanel.transform.localPosition = pos; 
+
+				if( m_slideAnimationTimer >= m_slideAnimationDuration)
+				{
+					EndSlideAnimation();
+				}
+			}
+		}
+
+		#region SuccessState
+
+		public enum CapsuleSuccessState {
+			Bad,
+			Neutral,
+			Good
+		}
+
+		void SetBorderMaterial(Image target, CapsuleSuccessState state = CapsuleSuccessState.Neutral)
+		{
+			Material appliedMaterial = null;
+			switch (state)
+			{
+				case CapsuleSuccessState.Bad:
+					appliedMaterial = m_badMaterial;
+					break;
+				case CapsuleSuccessState.Neutral:
+					appliedMaterial = m_neutralMaterial;
+					break;
+				case CapsuleSuccessState.Good:
+					appliedMaterial = m_goodMaterial;
+					break;
+			}
+			target.material = appliedMaterial;
+		}
+
+		public void SetFrontBorderState(CapsuleSuccessState state)
+		{
+			SetBorderMaterial(m_frontBorderImage, state);
+		}
+
+		public void SetBackBorderState(CapsuleSuccessState state)
+		{
+			SetBorderMaterial(m_backBorderImage, state);
+		}
+
+		#endregion
+	}
+}
---
+++ ProtoDash/Assets/Scripts/Tests/AnimationSandbox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Dasher;
+
+public class AnimationSandbox : MonoBehaviour {
+
+	public TimeDisplayCapsule m_timeCapsule;
+
+	void Start()
+	{
+		m_timeCapsule.Initialize();
+	}
+
+	void Update () {
+		m_timeCapsule.ManualUpdate();
+	}
+
+	public void StartFlash()
+	{
+		string randomMessage = "Time\n" + Random.Range(0, 99).ToString(TimeManager.c_timeDisplayFormat);
+
+		m_timeCapsule.FlashInText(randomMessage);
+	}
+
+	public void StartSlide()
+	{
+		m_timeCapsule.StartSlide();
+	}
+
+	public void HideAdditional()
+	{
+		m_timeCapsule.HideBackPanel();
+	}
+
+	public void SetFrontCapsuleState(int st)
+	{
+		m_timeCapsule.SetFrontBorderState((TimeDisplayCapsule.CapsuleSuccessState)st);
+	}
+
+	public void SetBackCapsuleState(int st)
+	{
+		m_timeCapsule.SetBackBorderState((TimeDisplayCapsule.CapsuleSuccessState)st);
+	}
+
+}
