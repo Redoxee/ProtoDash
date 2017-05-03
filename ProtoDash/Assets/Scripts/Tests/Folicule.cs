f7d81b3c7993dc3b9f2d1d2600150c0ec814fba8
Redoxee <anton.francis.roy@gmail.com>
2017-05-03 02:07:45 +0200

    Trying to add hairs to the character, that's not it

---
+++ ProtoDash/Assets/Scripts/Tests/Folicule.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Folicule : MonoBehaviour {
+	public GameObject m_hairPrefab = null;
+	public Transform m_childNode = null;
+
+	public uint BodyCount = 4;
+
+	public float StartDamp = .05f;
+	public float DampDecreaseFactor = .5f;
+
+	public float scaleDecreaseFactor = .8f;
+
+	private void Awake()
+	{
+		var currentDamp = StartDamp;
+		Transform currentTarget = m_childNode;
+		var currentScale = Vector3.one * scaleDecreaseFactor;
+
+		for (int i = 0; i < BodyCount; ++i)
+		{
+			var child = Instantiate(m_hairPrefab, currentTarget.position, currentTarget.rotation);
+			var hair = child.GetComponentInChildren<HairBehavior>();
+			hair.m_target = currentTarget;
+			hair.m_dampLoose = currentDamp;
+			hair.transform.localScale = currentScale;
+
+			currentTarget = hair.m_childNode;
+			currentDamp *= DampDecreaseFactor;
+			currentScale *= scaleDecreaseFactor;
+		}
+	}
+
+}
---
+++ ProtoDash/Assets/Scripts/Tests/HairBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairBehavior : MonoBehaviour {
+	public Transform m_childNode = null;
+	public Transform m_target = null;
+
+	public float m_dampLoose = 1f;
+
+
+	Vector3 m_velocity;
+	private void Update()
+	{
+		transform.position = Vector3.SmoothDamp(transform.position, m_target.position, ref m_velocity, m_dampLoose);
+		transform.localRotation = m_target.parent.localRotation;
+	}
+		
+}
