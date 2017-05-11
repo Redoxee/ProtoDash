b84d6f112f5f5c70cbf07a8d17bc51a6dcee9eb5
Redoxee <anton.francis.roy@gmail.com>
2017-05-12 01:37:23 +0200

    new iteration of the decorator elements

---
+++ ProtoDash/Assets/Scripts/Editor/DecoratorInspector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Dasher
+{
+	[CustomEditor(typeof(DecoratorHolder))]
+	public class DecoratorInspector : Editor
+	{
+		int m_northLength, m_eastLength, m_southLength, m_westLength = 1;
+
+		public override void OnInspectorGUI()
+		{
+			DecoratorHolder subject = target as DecoratorHolder;
+			base.OnInspectorGUI();
+
+			GUILink(subject.m_northLink, "N");
+			GUILink(subject.m_eastLink, "E");
+			GUILink(subject.m_southLink, "S");
+			GUILink(subject.m_westLink, "W");
+		}
+
+		void GUILink(GameObject link, string name)
+		{
+
+			bool isActive;
+			int length;
+			Vector3 scale;
+
+			GUILayout.BeginHorizontal();
+			isActive = link.activeSelf;
+
+			isActive = GUILayout.Toggle(isActive, name, GUILayout.Width(40));
+
+			scale = link.transform.localScale;
+			length = (int)scale.x;
+			if (GUILayout.Button("-", GUILayout.Width(20)))
+			{
+				length -= 1;
+			}
+			length = EditorGUILayout.IntField(length, GUILayout.Width(30));
+			if (GUILayout.Button("+", GUILayout.Width(20)))
+			{
+				length += 1;
+			}
+			if (length < 1)
+			{
+				length = 1;
+				isActive = false;
+			}
+
+			scale.x = length;
+			link.SetActive(isActive);
+			link.transform.localScale = scale;
+			
+			GUILayout.EndHorizontal();
+		}
+	}
+}
---
+++ ProtoDash/Assets/Scripts/Game/World/DecoratorHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dasher
+{
+	public class DecoratorHolder : MonoBehaviour
+	{
+		public GameObject m_northLink = null;
+		public GameObject m_eastLink = null;
+		public GameObject m_southLink = null;
+		public GameObject m_westLink = null;
+	}
+}
